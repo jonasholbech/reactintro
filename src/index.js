c76970e809084b4ad9478a2f39d04322ad180616
Jonas Holbech <jh@jonasholbech.dk>
2020-04-30 22:35:49 +0200

    that went pretty well

--- src/index.js
+++ src/index.js
@@ -2,4 +2,69 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<h1>Hi mom</h1>, document.getElementById("root"));
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Main />
+    </div>
+  );
+}
+function Nav() {
+  return <nav>Nav</nav>;
+}
+
+function Main() {
+  return (
+    <main>
+      <List header="ToDo" />
+      <List header="Doing" />
+      <List header="Done" />
+    </main>
+  );
+}
+
+function List(props) {
+  console.log(props);
+  return (
+    <section>
+      <h2>{props.header}</h2>
+      <ul>
+        <Card />
+        <Card />
+      </ul>
+      <Form />
+    </section>
+  );
+}
+function Card() {
+  return (
+    <li>
+      <h3>Header</h3>
+      <Button />
+    </li>
+  );
+}
+function Button() {
+  return <button>Click me</button>;
+}
+function Form() {
+  return (
+    <form>
+      <input />
+      <Button />
+    </form>
+  );
+}
+ReactDOM.render(<App />, document.getElementById("root"));
+
+/*
+Build the structure of the YAKAPP
+
+And while you do so, practice
+
+    Creating components
+    Nesting components
+    Setting props
+    Styling components (where do you put the className, and the CSS
+ */
