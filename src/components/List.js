3f8c3911c33500a79b2566b1078d5f5903b85565
Jonas Holbech <jh@jonasholbech.dk>
2020-05-01 16:11:15 +0200

    somewhat well i guess

--- src/components/List.js
+++ src/components/List.js
@@ -3,7 +3,7 @@ import Card from "./Card";
 import Form from "./Form";
 
 export default function List(props) {
-  const cards = props.cards.map((card) => <Card {...card} />);
+  const cards = props.cards.map((card) => <Card key={card.id} {...card} />);
   return (
     <section>
       <h2>{props.header}</h2>
